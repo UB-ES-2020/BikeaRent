148bd97a5266a919f21e4e630b38efc43a31e839
elenamora <32061288+elenamora@users.noreply.github.com>
2020-10-23 19:24:49 +0200

    Add files via upload

---
+++ backend/models/account.py
@@ -0,0 +1,16 @@
+from db import db
+from flask import g, current_app
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from flask_httpauth import HTTPBasicAuth
+from flask import g
+
+auth = HTTPBasicAuth()
+
+class AccountsModel(db.Model):
+
+    def __init__(self, username, available_money=200, is_admin=0):
+        self.username = username
+        self.available_money = available_money
+        self.is_admin = is_admin
+
