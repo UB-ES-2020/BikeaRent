bb39b9a286a24ae626e41ab0653d9160498937c1
elenamora <hanele96@gmail.com>
2020-11-02 18:35:02 +0100

    arreglos

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,18 @@
 from sqlite3.dbapi2 import Date
 
 from flask import Flask, render_template
-from flask_migrate import Migrate, Resource, Api
+from flask_migrate import Migrate
 from backend.models.booking import BookingModel
 from backend.models.moto import MotosModel
 from backend.models.account import AccountsModel
 from flask_restful import Resource, Api, reqparse
 from backend.db import db
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,7 +29,7 @@ api = Api(app)
 @app.route('/')
 @app.route('/userlogin')
 @app.route('/newaccount')
-def login():
+def render_vue():
     return render_template("index.html")
 
 
@@ -100,7 +102,7 @@ class Login(Resource):
 
 class MotosList(Resource):
     def get(self):
-        motos = MotosModel.query.filter_by(active = True)
+        motos = MotosModel.query.filter_by(active=True)
         motosList = []
         for moto in motos:
             motosList.append(moto.json())
