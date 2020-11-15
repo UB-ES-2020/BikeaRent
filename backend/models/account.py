4f61f936654eb0334070058889bd82ade3fd6f33
elenamora <hanele96@gmail.com>
2020-11-15 11:52:35 +0100

    registre

--- backend/models/account.py
+++ backend/models/account.py
@@ -24,7 +24,7 @@ class AccountsModel(db.Model):
     status = db.Column(db.Integer, nullable=False)
     creditCard = db.Column(db.String(30), unique=True, nullable=False)
     availableMoney = db.Column(db.Integer, nullable=False)
-    account_type = db.Column(db.Enum(*type, name='user_types'), nullable=False)
+    account_type = db.Column(db.Enum(*account_type, name='account_types'), nullable=False)
 
     def __init__(self, firstname, surname, email, username, dni, dataEndDrivePermission, creditCard,
                  account_type, status=0, availableMoney=100):
