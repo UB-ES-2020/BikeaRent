e405cdd3a42222807e45bea6812d968509b3e855
albertRoyo <aroyomar7@alumnes.ub.edu>
2020-11-15 20:06:31 +0100

    Task 1, task 2 and task 3 from US8 done in localhost

--- frontend/src/components/LlistatMotos.vue
+++ frontend/src/components/LlistatMotos.vue
@@ -10,15 +10,11 @@
     top: 0;
     width: 100%;
   }
-  .floated {
-    float:left;
-    margin-right:5px;
-  }
 </style>
 
 <template>
 <div id="app">
-  <div v-if="!navigation">
+  <div v-if="!navigation & !bikeAdding">
     <nav class="navbar navbar-dark">
       <h2 style="color: #d3d9df">BaikaRent</h2>
       <div>
@@ -26,6 +22,9 @@
         <h6 style="color: #d3d9df">{{this.user.money_available}} €</h6>
       </div>
     </nav>
+    <div v-if="user.type = 'support'" >
+      <button style="position: absolute; right: 0%" class="btn btn-warning"  @click="bikeAdding=true">Add Bike</button>
+    </div>
     <table>
       <thead style="border-bottom: 5px solid #000;">
         <tr>
@@ -40,7 +39,7 @@
       </tbody>
     </table>
   </div>
-  <div v-else>
+  <div v-if="navigation">
     <h3>Go to {{this.bike.street}} to unlock your bike.</h3>
     <div> Once you ara next to the bike, press the Unlock button to start the renting</div>
     <br>
@@ -50,6 +49,49 @@
     <h1>Time is running!!</h1>
     <div> Once you have stoped the bike, press the Lock button to end the renting</div>
     <button class="btn btn-danger" @click="lockBike">Lock </button>
+  </div>
+  <div v-if="bikeAdding">
+    <b-card style="width:250px; margin:auto">
+      <h3> Add a bike in the system</h3>
+      <button class="btn btn-outline-dark btn-sm" style="margin-block-end: 10px; position:absolute;top:0;right:0;" @click="bikeAdding=false">Close</button>
+      <b-form-group id="input-group-3" label="Model:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="bike.model"
+          required
+          placeholder="Enter the bike model"
+          >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-4" label="Charge:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="bike.charge"
+          required
+          placeholder="Enter the bike charge"
+          >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-5" label="Latitude:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="bike.latitude"
+          required
+          placeholder="Enter the bike latitude"
+          >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-6" label="Longitude:" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="bike.longitude"
+          required
+          placeholder="Enter the bike longitude"
+          >
+        </b-form-input>
+      </b-form-group>
+      <button class="btn btn-danger" @click="addBike, bikeAdding=false">Add this bike</button>
+    </b-card>
   </div>
 </div>
 </template>
@@ -63,16 +105,20 @@ export default {
         id: 0,
         token: null,
         username: 'Pene',
-        money_available: 69
+        money_available: 69,
+        type: ''
       },
       bike: {
-        id: 0,
-        battery: 100,
         model: '',
-        street: ''
+        active: false,
+        charge: '',
+        latitude: '',
+        longitude: ''
       },
+      supportLogged: true,
       navigation: false,
-      active: false
+      active: false,
+      bikeAdding: false
     }
   },
   methods: {
@@ -140,6 +186,25 @@ export default {
           console.error(error)
           alert('Sorry, you cannot take this bike. Try again')
         })
+    },
+    addBike () {
+      const path = 'https://bikearent4.herokuapp.com/bike'
+      const parameters = {
+        model: this.bike.model,
+        active: true,
+        charge: this.bike.charge,
+        latitude: this.bike.latitude,
+        longitude: this.bike.longitude
+      }
+      axios.post(path, parameters, {
+        auth: {username: this.user.token}
+      })
+        .then((res) => {
+          this.user = res.data.user
+        })
+        .catch((error) => {
+          console.error(error)
+        })
     }
   },
   created () {
@@ -148,6 +213,7 @@ export default {
     this.user.username = this.$route.query.username
     this.user.money_available = this.$route.query.token
     this.user.token = this.$route.query.token
+    this.user.type = this.$route.query.type
   }
 }
 </script>
