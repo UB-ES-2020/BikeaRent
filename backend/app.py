c1311fdd53bc33bb8f75e573a9f8607f39437b1f
Víctor Herrera <vherrera@redpoints.com>
2020-12-01 19:25:21 +0100

    Creado método PUT para modificar motos

--- backend/app.py
+++ backend/app.py
@@ -43,7 +43,7 @@ class Motos(Resource):
     parser.add_argument('charge', type=int, required=True, help="This field cannot be left blank")
     parser.add_argument('latitude', type=float, required=True, help="This field cannot be left blank")
     parser.add_argument('longitude', type=float, required=True, help="This field cannot be left blank")
-    parser.add_argument('plate',type=str,required = True,help = "This field cannot be left blank")
+    parser.add_argument('plate', type=str, required=True, help="This field cannot be left blank")
 
     def get(self, id):
         moto = MotosModel.find_by_id(id)
@@ -60,6 +60,18 @@ class Motos(Resource):
             return "Moto created successfully", 201
         except:
             return {"Error": "An error occurred creating moto"}, 500
+
+    def put(self, id):
+        data = self.parser.parse_args()
+
+        bike = MotosModel.find_by_id(id)
+        if bike:
+            modified_bike = MotosModel(**data)
+            if bike.model == modified_bike.model and bike.active == modified_bike.active and bike.charge == modified_bike.charge and bike.latitude == modified_bike.latitude and bike.longitude == modified_bike.longitude and bike.plate == modified_bike.plate:
+                return {"Error": "Bike {} is up to date".format(bike.plate)}, 400
+            MotosModel.modify_bike(id, modified_bike)
+            return {"bike": bike.json()}, 200
+        return {"Error": "Event with identifier {} not found".format(id)}, 404
 
 
 # -------- Register  ---------------------------------------------------------- #
